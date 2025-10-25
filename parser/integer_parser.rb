4d40c1090685f748b6faef2d6af589bd60f37818
hisakey <hogehoge@example.com>
2025-10-26 05:53:13 +0900

    feat: add simple sum

--- parser/integer_parser.rb
+++ parser/integer_parser.rb
@@ -17,10 +17,10 @@ class Lexer
 
   def next_token
     skip_whitespace
-    @token = if @scan.scan(/\d+/)
-               @scan.matched
+    @token = if n = @scan.scan(/\d+/)
+               [:DIGIT, n]
              elsif @scan.scan(/\+/)
-               :PLUS
+               [:PLUS, '+']
              end
   end
 
@@ -32,24 +32,44 @@ end
 class Parser
   def initialize(value)
     @lexer = Lexer.new(value)
+    @cur_pos = nil
   end
 
   def parse
-    @lexer.next_token
-    parse_integer
+    advance
+    parse_expr
   end
 
   private
 
-  def parse_integer
-    @lexer.token.to_i
+  def parse_expr
+    left = parse_int
+    advance
+    if match?(:PLUS)
+      advance
+      right = parse_int
+      left + right
+    else
+      left
+    end
+  end
+
+  def parse_int
+    @cur_pos[1].to_i
+  end
+
+  def advance
+    @cur_pos = @lexer.next_token
+  end
+
+  def match?(type)
+    @cur_pos && @cur_pos[0] == type
   end
 end
 
-# puts Parser.new(1).parse # => 1
-# puts Parser.new('1').parse # => 1
-# puts Parser.new(' 1 ').parse # => 1
-# puts Parser.new(' 234 ').parse # => 234
-# puts Parser.new(234).parse # => 234
+puts Parser.new('1').parse # => 1
+puts Parser.new(' 1 ').parse # => 1
+puts Parser.new(' 234 ').parse # => 234
+puts Parser.new(234).parse # => 234
 puts Parser.new('1 + 1').parse # => 2
 puts Parser.new('1 + 3').parse # => 4
