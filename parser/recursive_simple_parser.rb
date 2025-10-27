57923db9e0c4b68b3c7e0d9581149555f327fdcc
hisakey <hogehoge@example.com>
2025-10-28 06:04:28 +0900

    feat: implement modify

---
+++ parser/recursive_simple_parser.rb
@@ -0,0 +1,101 @@
+require 'strscan'
+
+# Token:
+#   DIGIT  ::= [0-9]+
+#   WS   ::= \s+
+#   PLUS ::= '+'
+#
+# expr   ::= term ( (PLUS | MINUS) term )*
+# term   ::= factor ( TIMES )  factor )*
+# factor ::= (PLUS | MINUS) factor | INT
+
+class Lexer
+  def initialize(value)
+    @scan = StringScanner.new(value.to_s)
+    @token = nil
+  end
+
+  attr_reader :token
+
+  def next_token
+    skip_whitespace
+    @token = if n = @scan.scan(/\d+/)
+               [:DIGIT, n]
+             elsif @scan.scan(/\+/)
+               [:PLUS, '+']
+             elsif @scan.scan(/-/)
+               [:MINUS, '-']
+             elsif @scan.scan(/\*/)
+               [:TIMES, '*']
+             end
+  end
+
+  def skip_whitespace
+    @scan.skip(/\s+/)
+  end
+end
+
+class Parser
+  def initialize(value)
+    @lexer = Lexer.new(value)
+    @cur_pos = nil
+  end
+
+  def parse
+    advance
+    parse_expr
+  end
+
+  private
+
+  def parse_expr
+    left = parse_term
+    while match?(:PLUS) || match?(:MINUS)
+      op = @cur_pos[0]
+      advance # 演算子を進める
+      rhs = parse_term
+      left = op == :PLUS ? left + rhs : left - rhs
+    end
+    left
+  end
+
+  def parse_term
+    left = parse_factor
+    while match?(:TIMES)
+      advance # 演算子を進める
+      right = parse_factor
+      left *= right
+    end
+    left
+  end
+
+  def parse_factor
+    v = @cur_pos[1].to_i
+    advance
+    v
+  end
+
+  def advance
+    @cur_pos = @lexer.next_token
+  end
+
+  def match?(type)
+    @cur_pos && @cur_pos[0] == type
+  end
+end
+
+# puts Parser.new('1').parse # 1
+# puts Parser.new(' 1 ').parse # => 1
+# puts Parser.new(' 234 ').parse # => 234
+# puts Parser.new(234).parse # => 234
+# puts Parser.new('1 + 1').parse # => 2
+# puts Parser.new('1 + 3').parse # => 4
+# puts Parser.new('1 + 1 + 1').parse # => 3
+# puts Parser.new('10 + 10 + 10').parse # => 30
+# puts Parser.new('50 - 20 + 10').parse # => 40
+
+# puts Parser.new('2 * 3').parse          # => 6
+puts Parser.new('1 + 2 * 3').parse # => 7   (1 + (2*3))
+# puts Parser.new('2 * 3 + 4').parse      # => 10  ((2*3) + 4)
+# puts Parser.new('2 + 3 * 4 + 5').parse  # => 19
+# puts Parser.new('2 * 3 * 4').parse      # => 24  (左結合)
